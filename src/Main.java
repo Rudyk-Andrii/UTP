51d52d46cf2f0d896cbb25e9cf0de470aa9894ff
Rudyk-Andrii <s30241@pjwstk.edu.pl>
2024-10-11 14:14:49 +0200

    Update Main.java
    Added your current task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+// TODO: we need to add the missing classes!
 public class Main {
     public static void main(String[] args){
         Adder adder = new Adder();
         System.out.println(adder.add(1,2));
+        
         Substractor substractor = new Substractor();
         System.out.println(substractor.substract(6,3));
     }
