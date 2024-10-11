494c53da2f024597a8fef6c7ca258ea87c4ede58
Z0d1akAllSeeingEye <andriirudyk32@gmail.com>
2024-10-11 14:17:14 +0200

    Adder

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-//OK I will add "Adder" and s30930 will ad the "Substractor"
+//OK I will add "Adder" and s30930 will ad the "Substractor".
 public class Main {
     public static void main(String[] args){
         Adder adder = new Adder();
